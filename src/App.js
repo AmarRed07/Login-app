dca446ebd395931284b3baa0a66972d7d51ba8fe
AmarRed07 <redzepagicamar@yahoo.com>
2021-03-06 16:46:52 -0800

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import LoginForm from './components/LoginForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const adminUser = {
+    email: "givemeanA@please.com",
+    password: "givemeanA"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Welcome, you've successfully logged in!");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("Incorrect! Try again.");
+      setError("Incorrect! Try again.")
+    }
+  }
+
+  const Logout = () => {
+    setUser({ name: "", email: ""});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="App" className="container">
+       <nav className="navbar navbar-expand sm navbar-dark bg-dark">
+        <a className="navbar-brand" href="placeholder.html">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+        <div className="collapse navbar-collapse" id="navbarNavBardropdown">
+            <ul className="navbar-nav">
+                <li className="nav-item active">
+                <a className="nav-link" href="placeholder.html"> This </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="placeholder.html"> Is </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="placeholder.html"> A </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="placeholder.html"> Navbar </a>
+                </li>
+            </ul>
+        </div>
+    </nav><br></br>
+      {(user.email !="") ? (
+        <div className="welcome">
+          <h2>Thanks for the grade! You promised! No going back now <span>{user.name}</span> :) </h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
 
 export default App;
+
